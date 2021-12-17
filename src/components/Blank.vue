712ea4fee0e3faca8214b3deef8083bf8e3a54c2
YangLyuJimmy <jimmylyuyang@outlook.com>
2021-12-16 19:22:20 -0500

    123

--- src/components/Blank.vue
+++ src/components/Blank.vue
@@ -1,9 +1,12 @@
 <template>
-  <v-card max-width=400px>
-    <v-card-title> This is currently a blank page! </v-card-title>
-    <v-btn @click="sendRequest">Test</v-btn>
-    Test message is : {{ testMessage }}
-  </v-card>
+  <div>
+    <v-card max-width=400px>
+      <v-card-title> This is currently a blank page! </v-card-title>
+      <v-btn @click="sendRequest">Test</v-btn>
+      Test message is : {{ testMessage }}
+    </v-card>
+    <v-btn>one Button</v-btn>
+  </div>
 </template>
 
 <script>
